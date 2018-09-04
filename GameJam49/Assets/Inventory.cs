4e95255492fc160912fc0f8313f7fac6c1d0b88e
Luc Schuttel <luc_schuttel@hotmail.com>
2018-09-04 16:27:44 +0200

    inventory added

--- GameJam49/Assets/Inventory.cs
+++ GameJam49/Assets/Inventory.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public int potions;
+    public int logs = 0, fish = 0, birds = 0, swords = 0, shields = 0, Rswords = 0, Rshields = 0, gp = 200;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
