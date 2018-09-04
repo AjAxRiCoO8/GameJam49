e1861e56b9b5107b9fd3284c3422bbf16546777f
Unknown <nickyking_1_@hotmail.com>
2018-09-04 14:43:38 +0200

    Added enums/structure to inventory / UI + few base functions
    - Structure to UI and inventory
    - Switching scenes through a tap on a button
    - UI stat text will change depending on the player stats
    - Enums directionary added

---
+++ GameJam49/Assets/HubWorldController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Enums;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubWorldController : MonoBehaviour
+{
+
+    //public Activities tapActivity;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        // All inputs needs to be frame based 
+	    TouchInput();
+
+	}
+
+    void TouchInput()
+    {
+        // Could be used for gesture navigation
+        //Touch touch = Input.GetTouch(0);
+    }
+
+    // Scene switcher when player touch a button
+    public void SceneSwitcher(Activities tapActivity)
+    {
+        switch (tapActivity)
+        {
+            case Activities.Fighting:
+                //Add scene on top of this scene
+                SceneManager.LoadScene(1);
+                break;
+            case Activities.Training:
+                //Add scene on top of this scene
+                break;
+        }
+        // Reset for the next tap
+        tapActivity = Activities.None;
+    }
+}
---
+++ GameJam49/Assets/UIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    public Text hpText,staText,strText;
+    Character characterController;
+
+    //List<int> statNumberList = new List<int>();
+    // Use this for initialization
+    void Start ()
+	{
+	    characterController = GameObject.Find("Player").GetComponent<Character>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    UpdateText();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateText()
+    {
+        hpText.text = "HP: " + Mathf.FloorToInt(characterController.health);
+        staText.text = "Sta: " + Mathf.FloorToInt(characterController.stamina);
+        strText.text = "Str: " + Mathf.FloorToInt(characterController.strength);
+    }
+
+    void GetStats()
+    {
+
+    }
+}
